3bd7d02f9fcb6f99c7916047a1820ce984bd3e12
Achraf-Assal <achrafassa.le4@gmail.com>
2024-01-04 23:24:59 +0100

    finish the auth logic and turn to web scraping

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,9 @@ import UserAgentRoute from "./routes/TestRout";
 import UserRoute from "./routes/UserRouts";
 import { requireAuth ,checkUser } from "./middleware/AuthMiddleware";
 const cookieParser = require('cookie-parser');
-const path = require('path');
 
 const app = express();
 // middleware
-app.use(express.static("public"));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -18,10 +16,6 @@ app.use(cookieParser());
 // config dotenv package to use .env varabels
 dotenv.config();
 
-// view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 const MongoUrl = process.env.MONGO_DB_URL as String || 3000;
 const PORT = process.env.PORT as unknown as Number;
@@ -38,10 +32,5 @@ mongoose.connect(MongoUrl as string)
 
     app.get("*",checkUser);
     // routes
-    app.get('/', (req, res) => res.render('home'));
-    app.get('/smoothies',requireAuth, (req, res) => res.render('smoothies'));
-    app.get('/login',(req, res) => res.render('login'));
-    app.get('/signup',(req, res) => res.render('signup'));
-
     app.use("/api/user-agent",UserAgentRoute);
     app.use("/api/user",UserRoute);
